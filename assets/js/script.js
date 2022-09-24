7085d004858c73b2595fc4c96e879e386914498c
Ewie1 <shephvo12@gmail.com>
2022-09-24 14:27:32 +0000

    Add empty string value

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,6 +23,10 @@ document.addEventListener("DOMContentLoaded", function() {
 
 function runGame(gameType){
     //Create two random numbers between 1 and 25
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus;
+
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
 
